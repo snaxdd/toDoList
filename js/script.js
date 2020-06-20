0aebdfcf2bc32e19991f9a5711c3c06e688ddc84
Nikita Shabulin <nwpress@yandex.ru>
2020-06-20 22:36:41 +0700

    Задание завершено

--- js/script.js
+++ js/script.js
@@ -1,1 +1,77 @@
 "use strict";
+
+const todoControl = document.querySelector(".todo-control"),
+    headerInput = document.querySelector(".header-input"),
+    todoList = document.querySelector(".todo-list"),
+    todoCompleted = document.querySelector(".todo-completed");
+
+const addToStorage = function(obj) {
+    const todoJSON = JSON.stringify(obj);
+    
+    localStorage.setItem("list", todoJSON);
+};
+
+const readStorage = function() {
+    const returnObj = JSON.parse(localStorage.getItem("list"));
+
+    return returnObj;
+};
+
+const todoData = readStorage();
+
+const render = function() {
+    todoList.textContent = "";
+    todoCompleted.textContent = "";
+
+    todoData.forEach(function(item) {
+        const li = document.createElement("li");
+        li.classList.add("todo-item");
+
+        li.innerHTML = "<span class=\"text-todo\">" + item.value + "</span>" +
+            "<div class=\"todo-buttons\">" + 
+                "<button class=\"todo-remove\"></button>" + 
+                "<button class=\"todo-complete\"></button>" + 
+            "</div>";
+
+        if (item.completed) {
+            todoCompleted.append(li);
+        } else {
+            todoList.append(li);
+        }
+        
+        const complete = li.querySelector(".todo-complete"),
+            remove = li.querySelector(".todo-remove");
+
+        complete.addEventListener("click", function() {
+            item.completed = !item.completed;
+            addToStorage(todoData);
+            render();
+        });
+        
+        remove.addEventListener("click", function() {
+            todoData.splice(todoData.indexOf(item), 1);
+            addToStorage(todoData);
+            render();
+        });
+    });
+};
+
+todoControl.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const newTodo = {
+        value: headerInput.value,
+        completed: false
+    };
+
+    if (newTodo.value.trim() !== "") {
+        todoData.push(newTodo);
+        headerInput.value = "";
+        addToStorage(todoData);
+    }
+
+    render();
+});
+
+render();
+
